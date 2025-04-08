1b9e62d97d7eaa3bc115d07b828823f4fc0051ee
Istiak Tridip <13367189+istiak-tridip@users.noreply.github.com>
2025-04-09 01:30:10 +0600

    feat: extend  base `Sqids` to skip  re-shuffling

---
+++ src/Sqids.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Istiak\Sqids;
+
+use Sqids\Sqids as BaseSqids;
+
+class Sqids extends BaseSqids
+{
+    public function __construct(string $alphabet = self::DEFAULT_ALPHABET, int $minLength = self::DEFAULT_MIN_LENGTH, array $blocklist = self::DEFAULT_BLOCKLIST)
+    {
+        parent::__construct($alphabet, $minLength, $blocklist);
+
+        /**
+         * The base Sqids library automatically shuffles the provided alphabet.
+         * However, since this implementation uses a pre-shuffled alphabet,
+         * we override the default behavior and skip the re-shuffling.
+         */
+        $this->alphabet = $alphabet;
+    }
+}
