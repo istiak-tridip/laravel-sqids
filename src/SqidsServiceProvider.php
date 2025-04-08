b562eb8e83301f0b1eceaa0b81463306440fa6fb
Istiak Tridip <13367189+istiak-tridip@users.noreply.github.com>
2025-04-08 23:01:10 +0600

    Initialize

---
+++ src/SqidsServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Istiak\Sqids;
+
+use Illuminate\Support\ServiceProvider;
+
+class SqidsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
