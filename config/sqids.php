b82668be602418172aabedd4039d8ab1902e30fb
Istiak Tridip <13367189+istiak-tridip@users.noreply.github.com>
2025-04-09 01:54:15 +0600

    feat: package config

---
+++ config/sqids.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+use Istiak\Sqids\Sqids;
+
+return [
+    /**
+     * This value is used to shuffle the alphabet for generating IDs, ensuring they
+     * remain unique to your application. In most cases, this value doesn't need
+     * to be changed, as the already unique app key is used by default.
+     *
+     * Be cautious: changing this will invalidate all previously generated IDs.
+     *
+     * @see https://sqids.org/faq#unique
+     */
+    'shuffle_seed' => env('SQIDS_SHUFFLE_SEED', env('APP_KEY')),
+
+    /**
+     * Specify the minimum length for generated IDs. All IDs will meet or exceed
+     * this length, ensuring visual consistency and making it harder to infer
+     * the original sequence length. Adjust this value to suit your needs.
+     *
+     * @see https://sqids.org/faq#minlength
+     */
+    'min_length' => env('SQIDS_MIN_LENGTH', 10),
+
+    /**
+     * Due to the design of the Sqids algorithm, multiple IDs can decode into
+     * the same sequence of numbers. When enabled, IDs are validated after
+     * decoding to ensure they are canonical.
+     *
+     * @see https://sqids.org/faq#valid-ids
+     */
+    'canonical_ids' => env('SQIDS_CANONICAL_IDS', true),
+
+    /**
+     * Provide a list of words that should not appear in generated Sqids.
+     * This is useful for preventing offensive or inappropriate terms
+     * from being included in your application's IDs.
+     *
+     * @see https://sqids.org/faq#why-blocklist
+     */
+    'blocklist' => Sqids::DEFAULT_BLOCKLIST,
+
+    /**
+     * Define a custom alphabet for generating IDs. If left unset, the Sqids library's
+     * default alphabet will be used. Unless you need shorter, longer, or specific
+     * characters in your IDs, you can leave this value as is. The alphabet is
+     * always shuffled using the "shuffle seed."
+     *
+     * @see https://sqids.org/faq#shorter-longer
+     */
+    'alphabet' => env('SQIDS_ALPHABET', Sqids::DEFAULT_ALPHABET),
+];
--- src/SqidsServiceProvider.php
+++ src/SqidsServiceProvider.php
@@ -8,8 +8,15 @@ use Illuminate\Support\ServiceProvider;
 
 class SqidsServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/sqids.php', key: 'sqids');
+    }
+
     public function boot(): void
     {
-        //
+        $this->publishes([
+            __DIR__.'/../config/sqids.php' => config_path('sqids.php'),
+        ], 'sqids-config');
     }
 }
