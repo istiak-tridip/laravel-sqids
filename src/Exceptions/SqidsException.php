32ca49a8086c8c6130f2b49191aef4f7cc7e4291
Istiak Tridip <13367189+istiak-tridip@users.noreply.github.com>
2025-04-13 21:15:34 +0600

    feat: implement sqids encoder & decoder

---
+++ src/Exceptions/SqidsException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Istiak\Sqids\Exceptions;
+
+use Exception;
+
+class SqidsException extends Exception {}
--- src/Sqids.php
+++ src/Sqids.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Istiak\Sqids;
 
+use Arr;
+use Istiak\Sqids\Exceptions\SqidsException;
 use Istiak\Sqids\Support\Config;
 use Istiak\Sqids\Support\CustomSqids;
 
@@ -28,6 +30,29 @@ class Sqids
         $this->blocklist = Config::blockList();
         $this->validateIds = Config::validateIds();
         $this->alphabet = app(Config::class)->shuffledAlphabet($seed);
+    }
+
+    public function encode(int $id): string
+    {
+        return $this->sqids()->encode([$id]);
+    }
+
+    /**
+     * @throws SqidsException
+     */
+    public function decode(string $id): int
+    {
+        $decoded = Arr::first($this->sqids()->decode($id));
+
+        if ($decoded === null) {
+            throw new SqidsException('The given ID is invalid or could not be decoded.');
+        }
+
+        if ($this->validateIds && $id !== $this->encode($decoded)) {
+            throw new SqidsException('The decoded ID is not valid. While it may decode to a valid number, re-encoding produces a different ID.');
+        }
+
+        return $decoded;
     }
 
     protected function sqids(): CustomSqids
